99ed67e88fced45f7f571c41841e79fe3a86d991
thejagithub <thejanaravindu2000@gmail.com>
2025-06-02 09:55:49 +0530

    Added vercel web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Poppins } from "next/font/google"
 import './globals.css'
 import Script from 'next/script'
+import { Analytics } from "@vercel/analytics/next"
 
 const poppins = Poppins({
   subsets: ["latin"],
